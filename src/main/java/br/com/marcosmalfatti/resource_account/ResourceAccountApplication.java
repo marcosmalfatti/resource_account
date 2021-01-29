e45fef9c427e2147ee7af78be36f9e441b244443
Marcos Malfatti <marcos.malfatti@gmail.com>
2021-01-29 21:23:21 -0200

    Commit Inicial

---
+++ src/main/java/br/com/marcosmalfatti/resource_account/ResourceAccountApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcosmalfatti.resource_account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcosmalfatti/resource_account/ResourceAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcosmalfatti.resource_account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
